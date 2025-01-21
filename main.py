e34107394112a7d3065b623947def705c2ac2ac4
Alevel88 <ale.velasquez88@gmail.com>
2025-01-21 23:33:50 +0000

    A: Bootdev solution to exercise

--- main.py
+++ main.py
@@ -22,9 +22,6 @@ def count_char(string):
 
     return count_lett_dict
 
-def sort_on(dict):
-    return dict[num]
-
 def report(letters):
     print('--- Begin report of books/frankenstein.txt ---')
     sorted_words=sorted(letters.items(),key=lambda item: int(item[1]))
@@ -43,32 +40,3 @@ def main():
 
 
 main()
-
-
-#def main():
-#    book_path = "books/frankenstein.txt"
-#    text = get_book_text(book_path)
-#   num_words = get_num_words(text)
-#   print(f"{num_words} words found in the document")
-
-
-#def get_num_words(text):
-#    words = text.split()
-#    return len(words)
-
-#def get_chars_dict(text):
-#    chars = {}
-#    for c in text:
-#        lowered = c.lower()
-#        if lowered in chars:
-#            chars[lowered] += 1
-#        else:
-#            chars[lowered] = 1
-#    return chars
-
-#def get_book_text(path):
-#    with open(path) as f:
-#        return f.read()
-
-
-#main()
